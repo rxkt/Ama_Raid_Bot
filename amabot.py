cbfc442c597214b870e19f9cf4a22400a60955e1
Eric Liao <Eric Liao>
2017-07-22 13:12:01 -0400

    updated notification message, cleaned up code, added 2 more useful script functions

--- amabot.py
+++ amabot.py
@@ -1,27 +1,27 @@
 import discord
 import datetime
 import asyncio
-
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-
-scope = ['https://spreadsheets.google.com/feeds']
-
-
 from discord.ext import commands
+
+
 
 client = discord.Client()
 
+scope = ['https://spreadsheets.google.com/feeds']
 
 update_message = """
-Hello, please update your answers to Amaterasu's 30 man form. You can change your hours available or edit your characters. IF YOU REMOVE A CHARACTER, PLEASE PM XPECT ON DISCORD.
+Hello, please update your answers to Amaterasu's 30 man form. IF YOU REMOVE A CHARACTER, PLEASE PM XPECT ON DISCORD.
 
-If nothing changed, you still need to submit the form again (click SUBMIT on the form to update the timestamp).
+IF YOU DO NOT EDIT YOUR FORM, YOUR DATA FROM THE PREVIOUS WEEK WILL BE USED.
+"""
 
-If you already updated your form for next week, ignore this message.
+no_member_found = """Unable to find you in the spreadsheet. Please go to
 
-P.S
-Don't change your discord ID. The bot wil not PM you, your schedule can be messed up, and you risk getting yellow/orange cards. Change your nickname instead."""
+<%s>
+
+to sign up for raids.""" % "https://docs.google.com/forms/d/e/1FAIpQLSfr_P7wEhtq85H2mt7VO2_DETBhK-N-ihg7bBImcwsq88GOEQ/viewform"
 
 class amabot(discord.Client):
     """Cog that scrapes Amaterasu's Raid spreadsheets!"""
@@ -33,15 +33,18 @@ class amabot(discord.Client):
     def is_me(m):
         return m.author == client.user
 
+
+    #When called, notifies all users who have not updated their spreadsheet within the past week to do so.
     @commands.command(pass_context=True)
     @client.event
     async def notify(self, ctx):
-        """Mentions all people on the spreadsheet whose timestamps on their form responses are not updated for a period of time."""
+        """Mentions all people on the spreadsheet whose timestamps on their form responses are not updated for a period of time.""" 
 
+        server = ctx.message.server
         credentials = ServiceAccountCredentials.from_json_keyfile_name('data/spreadsheet/Ama30man-baa10dc23211.json', scope)
         gc = gspread.authorize(credentials)
-        sh = gc.open("Raid info spreadsheet")
-        wsh = sh.worksheet("Info Parse")
+        sh = gc.open("20man raid sheet")
+        wsh = sh.worksheet("InfoParse")
         await self.bot.say("opened sheet . . .")
         
         memberIndex = 2
@@ -70,7 +73,6 @@ class amabot(discord.Client):
                 if abs((prevDate-todayDate).days) > 1: #exclude people who have updated yesterday?
                     
                     memberList.append( name)
-                    server = ctx.message.server
                     member = server.get_member_named( wsh.cell(memberIndex,2).value.strip(' ') )
                     try:
                         if link != '':
@@ -81,8 +83,7 @@ class amabot(discord.Client):
                             
                     except:
                         await self.bot.say("Could not message " + name )
-                        unmessaged_list.append( name )
-                        
+                        unmessaged_list.append( name )          
             memberIndex+=1
 
 
@@ -91,59 +92,134 @@ class amabot(discord.Client):
         else:
             await self.bot.say("Everyone messaged.")
 
-    #test function.
+
+    #Sends a discord message to the user that called this function with their respective link to their google forms
     @commands.command(pass_context=True)
     @client.event
-    async def pm(self,ctx,user=discord.Member):
-        "Test function"
-        server = ctx.message.server
-        #test using IDs instead of name#discrim
-        member = server.get_member_named(user)
-        await self.bot.send_message(member.id, update_message)
+    async def link(self, ctx, *, user: discord.Member=None):
+        "Retrieves the link to the form for signing up."
+
+        author = ctx.message.author
+        if not user:
+            user = author
+
+        credentials = ServiceAccountCredentials.from_json_keyfile_name('data/spreadsheet/Ama30man-baa10dc23211.json', scope)
+        gc = gspread.authorize(credentials)
+        sh = gc.open("20man raid sheet")
+        wsh = sh.worksheet("InfoParse")
+        #debug
+        #await self.bot.say("opened sheet . . .")
+        try:
+            cell = wsh.find(str(user.id))
+            link = wsh.cell(cell.row,4).value
+            await self.bot.send_message(user, link)
+            await self.bot.say("Sent you the link, check your DMs <:blobshiro:331164843417665539>")
+        except:
+            if 'Amaterasu' in [str(i) for i in user.roles]:
+                await self.bot.send_message(user,no_member_found)
+            await self.bot.say("You are not on the spreadsheet for Harrowhold raids.")
+
+    @commands.command(pass_context=True)
+    @client.event
+    async def test(self, ctx, *, user: discord.Member=None):
+        "Test function for members"
+        author = ctx.message.author
+        if not user:
+            user = author
+            
+    #Sends a discord message to the user that called this function
+    #with their respective performance ratings
+    @commands.command(pass_context=True)
+    @client.event
+    async def pp(self, ctx, *, user:discord.Member=None):
+        "Returns your performance rating of all your characters for raids in Phase 4."
+
+        author = ctx.message.author
+        if not user:
+            user = author
+
+        credentials = ServiceAccountCredentials.from_json_keyfile_name('data/spreadsheet/Ama30man-baa10dc23211.json', scope)
+        gc = gspread.authorize(credentials)
+        sh = gc.open("20man raid sheet")
+        wsh = sh.worksheet("InfoParse")
+        
+        try:
+            cell = wsh.find(str(user.id))
+            name = wsh.cell(cell.row,3).value
+
+        except:
+            await self.bot.say("You are not on the spreadsheet for Harrowhold raids.")
+            return
+
+        await self.bot.say("Opened 'consolidated names'...")
+        wsh_names = sh.worksheet("Consolidated Names")
+        main_names = wsh_names.col_values(6)
+        row_index = main_names.index(name)+1
+        
+        alt_names = [item for item in wsh_names.row_values(row_index) if item!= ''][1:]
+        
 
         
-    #assigns the role given.
+        #deaths
+        #number of attempts
+        #pp, calculation
+        #if no runs-> no runs yet! 
+
+        
+    
+
+    #Assigns the discord IDs for each given discord tag on the spreadsheet
     @commands.command(pass_context=True)
-    async def iam(self, ctx: commands.Context, role: discord.Role ):
-        "List of possible roles :\n DPS, Healer, Tank, Priest, Mystic, Overwatch, ERP, League, Weeb. \n\n CASE SENSITIVE."
-        author = ctx.message.author
-        roles = [ 'DPS' , 'Healer', 'Tank' , 'Priest' , 'Mystic' , 'Overwatch' , 'ERP' , 'League' , 'Weeb' ]
-        for x in roles:
-            #.lower() is unneeded... but keeping for updated algo
-            if str(role).lower() == x.lower():
-                await self.bot.add_roles(author, role)
-                await self.bot.say(":ok: You now have the " + x + " role.")
-                return
+    @client.event
+    async def assign_ids(self, ctx, *, user: discord.Member=None):
+        "Updates the ID of every discord member on the spreadsheet."
+
+        server=ctx.message.server
         
-        await self.bot.say("Not a valid role. ")
+        credentials = ServiceAccountCredentials.from_json_keyfile_name('data/spreadsheet/Ama30man-baa10dc23211.json', scope)
+        gc = gspread.authorize(credentials)
+        sh = gc.open("20man raid sheet")
+        wsh = sh.worksheet("InfoParse")
+        await self.bot.say("opened sheet . . .")
+
+        memberIndex=2
+        while True:
+            timestamp = wsh.cell(memberIndex,1).value
+            discord_tag = wsh.cell(memberIndex, 2).value
+            name = wsh.cell(memberIndex, 3).value
+            if timestamp=='':
+                break
+            
+            member = server.get_member_named( wsh.cell(memberIndex,2).value.strip(' ') )
+            if member==None:
+                await self.bot.say("Did not update ID for %s" % name)
+            else:
+                wsh.update_cell(memberIndex, 5, member.id)
+                await self.bot.say("Updated ID for %s" % name)
+
+            memberIndex+=1
         
-    #useless for now, may want to put this into a PM. Lists all raids not yet occurred.
+        
+        
+    # Lists all raids not yet occurred. useless for now, may want to put this into a PM.
     @commands.command()
     async def raidtimes(self):
         "Lists all the raid that have not yet occurred."
+
+        await self.bot.say("Searching the spreadsheets for raids...")
+        
         credentials = ServiceAccountCredentials.from_json_keyfile_name('data/spreadsheet/Ama30man-baa10dc23211.json', scope)
         gc = gspread.authorize(credentials)
-        sh = gc.open("Raid info spreadsheet")
-        wsh = sh.worksheet("raid_lineup2")
+        sh = gc.open("20man raid sheet")
+        wsh = sh.worksheet("raid lineup")
 
         raidIndex = 1
-        raidList = list();
-        
-        #while there is still a raid, create a dict and add it to the list
-        while (wsh.cell(1,raidIndex).value != '' ):
-            raidDict = {}
-            raidDict['day']=wsh.cell(1,raidIndex).value
-            raidDict['date']=wsh.cell(1,raidIndex+1).value
-            raidDict['time']=wsh.cell(1,raidIndex+2).value
-            #add all tanks, dps, priest, mys as arrays to keys
-            
-            raidList.append(raidDict)
-            raidIndex+=5
         raidTimes=""
-
-        for each in raidList:
-            raidTimes+= str(raidList.index(each)+1) + ")\t" + each.get('date') + "\t@ " + each.get('time') + "\t(" + each.get('day') + ")\n"
         
+        while (wsh.cell(1,raidIndex).value != '' ):
+            raidTimes+= "%d) %s\n " % ( (raidIndex/5+1) , wsh.cell(1,raidIndex).value )
+            raidIndex+=5
+
         await self.bot.say("There are " + str( int(raidIndex/5) ) + " raids this week.\n" + raidTimes)
 
 def setup(bot):
